e7d72361af9b785e45c031504acfb0a3c7d70526
bwood3 <bryanwood1207@gmail.com>
2023-03-31 21:54:55 -0400

    404 error fix

--- src/main/java/com/example/tracking_service/controller/TrackingController.java
+++ src/main/java/com/example/tracking_service/controller/TrackingController.java
@@ -1,5 +1,6 @@
 package com.example.tracking_service.controller;
 
+import com.example.tracking_service.handler.ErrorResponse;
 import com.example.tracking_service.model.Invoice;
 import com.example.tracking_service.model.InvoiceItem;
 import com.example.tracking_service.model.Tracking;
@@ -7,6 +8,8 @@ import com.example.tracking_service.model.invoiceItemFields.Item;
 import com.example.tracking_service.repository.TrackingRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/tracking")
@@ -23,12 +26,28 @@ public class TrackingController {
     {
         Invoice i = repository.getTrackingOrderByID(orderID);
         if(i == null)
-            throw new IllegalStateException("404 error\norder id does not exist in the system");
+            throw new ResourceNotFoundException("order id does not exist in the system");
         InvoiceItem invoiceItem = i.getInvoiceItem();
         Item item = invoiceItem.findItemById(itemID);
         if(item == null)
-            throw new IllegalStateException("404 error\norder with item id does not exist in the system");
+            throw new ResourceNotFoundException("order with item id does not exist in the system");
         return item;
+    }
+
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
+    @ControllerAdvice
+    public class ExceptionHandlerController {
+
+        @ExceptionHandler(TrackingController.ResourceNotFoundException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        @ResponseBody
+        public ErrorResponse handleResourceNotFoundException(TrackingController.ResourceNotFoundException ex) {
+            return new ErrorResponse(ex.getMessage(), LocalDateTime.now());
+        }
     }
 
     @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "order with this id does not exist in the system")
